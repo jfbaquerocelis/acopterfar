4205760ae3fdc9ed443718f7bf4fd84403d371b2
John Baquero Celis <jfbaquerocelis@gmail.com>
2015-05-21 12:00:56 -0500

    [add] official page and [update] building page and favicon

---
+++ public/js/acopterfar.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+	$(window).scroll(function () {
+		if ($(window).scrollTop() > 400){
+			$('.buttonUp').fadeIn('slow')
+		}else{
+			$('.buttonUp').fadeOut('slow')
+		}
+		// console.log($('.buttonUp').offset().top)
+		if ($('.buttonUp').offset().top >= $('.Footer').offset().top){
+			$('.buttonUp-icon').css({
+					'color' : '#333'
+			})
+		}else{
+			$('.buttonUp-icon').css({
+				'color' : '#F44336'
+			})
+		}
+	})
+	$('.buttonUp-icon').click(function (e) {
+		e.preventDefault()
+		$('body').animate({
+			scrollTop : 0
+		}, 500)
+	})
+})
